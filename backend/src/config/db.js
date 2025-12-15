8ea269b0de7eb4d5a418908325e2535e4a756592
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-12-15 17:29:50 +0700

    update base backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
-    console.error(error);
+    console.error("❌ MongoDB error", err);
     process.exit(1);
   }
 };
