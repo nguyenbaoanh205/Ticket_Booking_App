a1e3ee029544cb3fd52defa64f9ffb777c63be92
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-12-27 00:12:50 +0700

    update crud Backend nodejs

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -5,6 +5,27 @@ exports.create = async (req, res) => {
   res.json(event);
 };
 
+exports.update = async (req, res) => {
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  });
+
+  if (!event) {
+    return res.status(404).json({ message: "Event not found" });
+  }
+
+  res.json(event);
+};
+
+exports.delete = async (req, res) => {
+  const event = await Event.findByIdAndDelete(req.params.id);
+  if (!event) {
+    return res.status(404).json({ message: "Event not found" });
+  }
+
+  res.json({ message: "Event deleted successfully" });
+};
+
 exports.getAll = async (req, res) => {
   const events = await Event.find();
   res.json(events);
--- backend/src/routes/event.route.js
+++ backend/src/routes/event.route.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
 const ctrl = require("../controllers/event.controller");
+const auth = require("../middlewares/auth.middleware");
+const isAdmin = require("../middlewares/isAdmin.middleware");
 
-router.post("/", ctrl.create);
+// Admin
+router.post("/", auth, isAdmin, ctrl.create);
+router.put("/:id", auth, isAdmin, ctrl.update);
+router.delete("/:id", auth, isAdmin, ctrl.delete);
+
+// Client
 router.get("/", ctrl.getAll);
-
 router.get("/:id", ctrl.getDetail);
 
 module.exports = router;
